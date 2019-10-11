f9497dd26e124595481e7e67c55206c4701ef4ee
Crankums <44502373+Crankums@users.noreply.github.com>
2019-10-11 00:03:38 -0500

    Adds scheduling helper method pseudocode

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,2 +1,4 @@
 class Workout < ApplicationRecord
+
+    
 end
--- db/migrate/20191010024404_create_calendars.rb
+++ db/migrate/20191010024404_create_calendars.rb
@@ -2,9 +2,12 @@ class CreateCalendars < ActiveRecord::Migration[6.0]
   def change
     create_table :calendars do |t|
       # look up how to create a rails calendar, whether it requires a gem or build one through methods
+      # weekly, monthly? i'm thinking weekly.
       t.integer :appointment_id
       t.integer :workout_id
       t.integer :user_id
+      t.string :days
+      t.datetime :date
       # calendar attributes
       t.timestamps # necessary?
     end
